1e81000074d551836e911c4a9057d842c489fc1d
OniReimu <reimu-saber@hotmail.com>
2018-05-07 20:18:40 +0800

    Update bootnode.sh

--- bootnode.sh
+++ bootnode.sh
@@ -5,5 +5,5 @@ if [ ! -f bootnode.key ];then
 	$abdir -genkey bootnode.key
 fi
 
-pkill $home/geth/ucot/build/bin/bootnode
+pkill ~/geth/ucot/build/bin/bootnode
 nohup $abdir -nodekey=bootnode.key --verbosity 5 -addr :30310 > bootnode.log &
